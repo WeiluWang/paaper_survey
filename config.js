32a33d30a4f7b51ef574f99891c56b63b1723f62
ed wang <ed@runtheworld.today>
2022-06-29 07:23:02 -0400

    add-Watch this: A taxonomy for dynamic data visualization

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const url = "papers.csv";
 const taxonomy = {
 
 	design_space:["workflow", "framework"],
-	animation:["survey", "3d"],
-	visualization:["physical", "method"],
+	animation:["2d", "3d", "physical"],
+	visualization:["survey", "method"],
 	// facet_a: ["red", "blue"],
 	// facet_b: ["pears", "apples", "oranges"],
 	// facet_c: ["alpha", "beta", "gamma", "delta"]
